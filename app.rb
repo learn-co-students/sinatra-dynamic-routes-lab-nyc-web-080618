a1ca23dc166e33fece8d5a433c420f4247a161aa
github username <github email address>
2018-08-27 17:20:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,52 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+
+  get '/square/:number' do
+    @num = params[:number].to_i
+    @square = @num ** 2
+    "#{@square}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    return_string = ""
+    @number.times do
+      return_string << @phrase
+    end
+    return_string
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation].to_s
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    if @operation == 'add'
+      return_val = @number1 + @number2
+    elsif @operation == 'subtract'
+      return_val = @number1 - @number2
+    elsif @operation == 'multiply'
+      return_val = @number1 * @number2
+    elsif @operation == 'divide'
+      return_val = @number1 / @number2
+    end
+    "#{return_val}"
+  end
 
 end
